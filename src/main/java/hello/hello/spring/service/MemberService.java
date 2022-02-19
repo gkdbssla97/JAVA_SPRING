d25e9aa5ec502c67b19a89b55fc678b7f67e37de
Hayoon <gkdbssla97@gmail.com>
2022-02-19 15:01:18 +0900

    SPRING

--- src/main/java/hello/hello/spring/service/MemberService.java
+++ src/main/java/hello/hello/spring/service/MemberService.java
@@ -3,6 +3,8 @@ package hello.hello.spring.service;
 import hello.hello.spring.domain.Member;
 import hello.hello.spring.repository.MemberRepository;
 import hello.hello.spring.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
